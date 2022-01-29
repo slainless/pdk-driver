17698447c2b0f4cd682df1fe0a5a332911a614cd
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-01-29 22:13:22 +0800

    test: add test for `driver` and `connection`

---
+++ test/connection.test.ts
@@ -0,0 +1,74 @@
+import test from 'ava'
+import * as imported from '../src/connection.js'
+import { Cookie, PHPCookieJar } from '../src/lib/cookie.js'
+import { readFileSync } from 'node:fs'
+import { FormData } from 'formdata-polyfill/esm.min.js'
+import { nanoid } from 'nanoid'
+
+const credential: imported.Credential = JSON.parse(
+  readFileSync('dev/credential.json', { encoding: 'utf-8' })
+)
+const genBody = () => {
+  const body = new FormData()
+  body.append('nmgp_opcao', 'ajax_navigate')
+  body.append('opc', 'rec') // receber/recibo/receita => get
+  body.append('parm', '1')
+  return body
+}
+
+test('Export the class by default', (t) => {
+  t.notThrows(() => new imported.default())
+})
+
+test('Connection will generate cookies on construction', (t) => {
+  const c = new imported.default()
+  t.true(c.cookieJar.length !== 0)
+  t.true(c.cookieJar instanceof PHPCookieJar)
+  t.true(c.cookieJar.sessionId instanceof Cookie)
+  t.true(c.cookieJar.sessionId?.name === 'PHPSESSID')
+})
+
+test('Connection can login with our own generated cookies', async (t) => {
+  const c = new imported.default()
+  await t.notThrowsAsync(() => c.login(credential))
+  t.true(c.isLoggedIn)
+  await t.notThrowsAsync(() =>
+    c
+      .fetch('/grid_agtkeluarga/index.php', {
+        body: genBody,
+        method: 'POST',
+      })
+      .then((r) => r.text())
+      .then((r) => {
+        t.true(r.indexOf('Pengguna yang tidak berizin') === -1)
+      })
+  )
+})
+
+test('Connection can autologin on demand when useCredential is set', async (t) => {
+  const c = new imported.default({
+    useCredential: credential,
+  })
+  await t.notThrowsAsync(() =>
+    c
+      .fetch('/grid_agtkeluarga/index.php', {
+        body: genBody,
+        method: 'POST',
+      })
+      .then((r) => r.text())
+      .then((r) => {
+        t.true(r.indexOf('Pengguna yang tidak berizin') === -1)
+        return
+      })
+  )
+})
+
+test('Connection will throw error on wrong credential', async (t) => {
+  const c = new imported.default({
+    useCredential: {
+      username: 'abc',
+      password: 'cde',
+    },
+  })
+  await t.throwsAsync(() => c.login())
+})
--- test/driver.test.ts
+++ test/driver.test.ts
@@ -1,8 +1,14 @@
 import test from 'ava'
-import { has, get } from 'lodash-es'
 import * as imported from '../src/driver.js'
-import Connection from '../src/connection.js'
+import Connection, { Credential } from '../src/connection.js'
+import { readFileSync } from 'node:fs'
+import { AnggotaKeluargaScheme } from '../src/lib/schema.js'
+import * as s from 'superstruct'
+import { get } from 'lodash-es'
 
+const credential: Credential = JSON.parse(
+  readFileSync('dev/credential.json', { encoding: 'utf-8' })
+)
 test('Driver export the class by default', (t) => {
   t.notThrows(
     () =>
@@ -14,11 +20,96 @@ test('Driver export the class by default', (t) => {
 })
 
 test('Driver initialize a Connection', (t) => {
-  const { default: Driver } = imported
-  const driver = new Driver({
+  const d = new imported.default({
     username: '',
     password: '',
   })
 
-  t.true(get(driver, 'connection') instanceof Connection)
+  t.true(d.connection instanceof Connection)
+})
+
+test('Driver can run multiple & asynchronous getDataPenduduk()', async (t) => {
+  const d = new imported.default(credential)
+
+  await t.notThrowsAsync(() =>
+    Promise.all([
+      d.getDataPenduduk({ page: 1 }),
+      d.getDataPenduduk({ page: 2 }),
+      d.getDataPenduduk({ page: 3 }),
+    ]).then((r) => {
+      for (const v of r) {
+        t.true(v.data.length === 15)
+        t.true(s.is(v.data, s.array(AnggotaKeluargaScheme)))
+      }
+    })
+  )
+})
+
+test('Driver can run multiple & asynchronous findDataPenduduk()', async (t) => {
+  const d = new imported.default(credential)
+  await t.notThrowsAsync(() =>
+    Promise.all([
+      d.findDataPenduduk('KODE', '7310110705680002'),
+      d.findDataPenduduk('KODE', '7310115506680007'),
+      d.findDataPenduduk('KODE', '7311021608790002'),
+    ]).then((r) => {
+      for (const v of r) {
+        t.true(s.is(v, AnggotaKeluargaScheme))
+        t.true(
+          s.is(
+            v,
+            s.type({
+              nik: s.union([
+                s.literal('_7310110705680002'),
+                s.literal('_7310115506680007'),
+                s.literal('_7311021608790002'),
+              ]),
+            })
+          )
+        )
+      }
+    })
+  )
+})
+
+test('Driver can run multiple method asynchronously', async (t) => {
+  const d = new imported.default(credential)
+  await t.notThrowsAsync(() =>
+    Promise.all([
+      d.getDataPenduduk({ page: 1 }),
+      d.getDataPenduduk({ page: 2 }),
+      d.findDataPenduduk('KODE', '7310110705680002'),
+      d.findDataPenduduk('KODE', '7310115506680007'),
+      d.findDataPenduduk('KODE', '7311021608790002'),
+    ]).then((r) => {
+      for (const i in r) {
+        if (['0', '1'].includes(i)) {
+          t.true(
+            s.is(
+              r[i],
+              s.object({
+                count: s.number(),
+                data: s.array(AnggotaKeluargaScheme),
+              })
+            )
+          )
+          t.true(get(r[i], 'data.length') === 15)
+        } else {
+          t.true(s.is(r[i], AnggotaKeluargaScheme))
+          t.true(
+            s.is(
+              r[i],
+              s.type({
+                nik: s.union([
+                  s.literal('_7310110705680002'),
+                  s.literal('_7310115506680007'),
+                  s.literal('_7311021608790002'),
+                ]),
+              })
+            )
+          )
+        }
+      }
+    })
+  )
 })
